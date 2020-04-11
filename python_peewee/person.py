35958b33c98de738c384db28667a6a4a5d83e30a
owncreed93 <christiantarifer@gmail.com>
2020-04-10 22:10:19 -0500

    creando modelo pet

--- python_peewee/person.py
+++ python_peewee/person.py
@@ -10,11 +10,19 @@ class Person(Model):
     class Meta:
         database = db
 
+class Pet(Model):
+    owner = ForeignKeyField(Person, backref='pets')
+    name = CharField()
+    animal_type = CharField()
+
+    class Meta:
+        database = db
+
 # * FUNCTION THAT CREATES THE DB AND ITS TABLES
 def create_and_connect():
     db.connect()
     # *  db.create_tables([<lists_with_tables]>, safe=True) --> this allows to create tables without worrying about that the ones already exists
-    db.create_tables([Person], safe=True)
+    db.create_tables([Person, Pet], safe=True)
 
 # * EXECUTE THE CREATION OF THE DB
 create_and_connect()
