21a20de17f21ac7bb65f81ed5fbfe95578992d3f
owncreed93 <christiantarifer@gmail.com>
2020-04-10 23:53:49 -0500

    anadiendo consultas de insercion y modificacion

--- python_peewee/person.py
+++ python_peewee/person.py
@@ -1,4 +1,5 @@
 from peewee import *
+from datetime import date
 
 db = SqliteDatabase('people.db')
 
@@ -24,5 +25,23 @@ def create_and_connect():
     # *  db.create_tables([<lists_with_tables]>, safe=True) --> this allows to create tables without worrying about that the ones already exists
     db.create_tables([Person, Pet], safe=True)
 
+def create_family_members():
+    # * INSERT DATA METHOD ONE
+    uncle_tommy = Person(name='Tommy', birthday=date(2000,11,11), is_relative=True)
+    uncle_tommy.save()
+
+    # * INSERT DATA METHOD TWO
+    grandma_ana = Person.create(name="Ana", birthday=date(1960,10,10), is_relative=True)
+
+    tommy_dog = Pet.create(owner=uncle_tommy, name="Fido", animal_type="Dog")
+    grandma_anas_cat = Pet.create(owner=grandma_ana, name="Peluza", animal_type="Cat")
+
+    tommy_dog.name = "Firulais"
+    tommy_dog.save()
+
+def create_family_pets():
+    tommy_dog = Pet()
+
 # * EXECUTE THE CREATION OF THE DB
 create_and_connect()
+create_family_members()
