2ba3ff540325eda354c5edbe199262a188ef4cb2
owncreed93 <christiantarifer@gmail.com>
2020-04-11 21:34:51 -0500

    borrado de datos

--- python_peewee/person.py
+++ python_peewee/person.py
@@ -32,16 +32,55 @@ def create_family_members():
 
     # * INSERT DATA METHOD TWO
     grandma_ana = Person.create(name="Ana", birthday=date(1960,10,10), is_relative=True)
+    grandma_rosa = Person.create(name="Rosa", birthday=date(1960,10,10), is_relative=True)
 
-    tommy_dog = Pet.create(owner=uncle_tommy, name="Fido", animal_type="Dog")
+    tommys_dog = Pet.create(owner=uncle_tommy, name="Fido", animal_type="Dog")
     grandma_anas_cat = Pet.create(owner=grandma_ana, name="Peluza", animal_type="Cat")
+    grandma_rosas_dog = Pet.create(owner=grandma_rosa, name="Firulais", animal_type="Dog")
 
-    tommy_dog.name = "Firulais"
-    tommy_dog.save()
+    #tommys_dog.name = "Firulais"
+    #tommys_dog.save()
 
-def create_family_pets():
-    tommy_dog = Pet()
+def get_family_pets():
+    print("*****************************************")
+    for pet in Pet.select():
+        print(f"Nombre: {pet.name}, Dueño: {pet.owner.name}, Raza: {pet.animal_type}")
+
+def get_family_members():
+    print("*****************************************")
+    for person in Person.select():
+        print(f"Nombre: {person.name}, Cumpleaños: {person.birthday}, Es familiar : {person.is_relative}")
+
+def get_family_member_birthday(n):
+    print("*******************************************")
+    # * SEARCH METHOD 1
+    # family_member = Person.select().where(Person.name == n).get()
+    # * SEARCH METHOD 2
+    family_member = Person.get(Person.name == n)
+    print(f"El cumpleaños de {family_member.name} es en la fecha {family_member.birthday}")
+
+def delete_person(per):
+    query = Person.delete().where(Person.name == per)
+    deleted_entries = query.execute()
+    print(f"{deleted_entries} registros borrados.")
+
+def delete_pet(n):
+    query = Pet.delete().where(Pet.name == n)
+    deleted_entries = query.execute()
+    print(f"{deleted_entries} registros borrados.")
+
+def delete_one_pet(n):
+    query = Pet.delete().where(Pet.name == n)
+    deleted_entry = query.delete_instance()
+    print(f"{deleted_entry} registros borrado.")
+
 
 # * EXECUTE THE CREATION OF THE DB
 create_and_connect()
-create_family_members()
+#create_family_members()
+get_family_members()
+get_family_pets()
+#get_family_member_birthday('Tommy')
+#delete_person("Tommy")
+#delete_person("Ana")
+#delete_pet("Firulais")
